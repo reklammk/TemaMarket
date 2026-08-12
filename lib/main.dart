f3c9171c4f120abadcf7c350f3f47f3b0c4e3e14
reklammk <mehmetp835@gmail.com>
2026-08-12 20:31:46 +0300

    fix: add dart:ui import and complete crash protection build

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'services/notification_service.dart';
 import 'pages/splash_screen_page.dart';
